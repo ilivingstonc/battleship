1b09b43f47b3ad6e971647d104d1b20d034539da
Ian Livingston <i.livingston.c@gmail.com>
2020-08-18 11:25:34 -0500

    first commit

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+//arrays
+const alphaArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+//global variables
+let pickTile = 0;
+
+// //jquery page toggle
+// $("startButton").click(function(){
+//     $("startContainer").toggle();
+// });
